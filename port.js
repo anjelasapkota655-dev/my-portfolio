0fcd3b7039b619c7cb11372135f4a153e07eb0dc
Anjela <anjelasapkota655@gmail.com>
2026-03-16 07:13:29 +0545

    Update port.js

--- port.js
+++ port.js
@@ -7,9 +7,11 @@ function scrollToSection(sectionId) {
 const mobileMenu = document.getElementById("mobile-menu");
 const navLinks = document.querySelector(".nav-links");
 
-mobileMenu.addEventListener("click", () => {
-  navLinks.classList.toggle("active");
-});
+if (mobileMenu && navLinks) {
+  mobileMenu.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+  });
+}
 
 // Form submission
 document.getElementById("contact-form").addEventListener("submit", function(e) {
